3a3ce19ea64a73b6af06420e79b7ce5a6f7046f6
grittaweisheit <grittaweisheit@gmail.com>
2024-03-13 11:02:47 +0100

    fix resources

--- pulp/fCM_schedule.py
+++ pulp/fCM_schedule.py
@@ -34,27 +34,30 @@ install_shower_b = 3
 install_toilet_s = 4
 install_toilet_b = 5
 install_kitchen = 6
+activity_names = ["start", "build_room", "install_shower_t", "install_shower_b", "install_toilet_s", "install_toilet_b", "install_kitchen", "finish"]
 
 duration = [1, 1, 2, 2, 1, 1, 2, 1]
-role_requirement = [0, 1, 2, 3, 3, 3, 4, 0]
+role_requirement = [0, 1, 2, 2, 3, 3, 4, 0]
 resource_consumption = [0, 1, 1, 1, 1, 1, 2, 0]
 ### resources ###
 
-resource_ids = [0, 1, 2, 3, 4]  # no resource, Bob, Sandy, Kay, Tina
-roles = [0, 1, 2, 3, 4]  # builder, shower-crew, toilet-crew, kitchen-crew
+resource_names = ["-", "Bob", "Sandy", "Kay", "Tina"]
+role_names = ["-", "builder", "shower-crew", "toilet-crew", "kitchen-crew"]
+resource_ids = [0, 1, 2, 3, 4]  # no resource / started, Bob, Sandy, Kay, Tina
+roles = [0, 1, 2, 3, 4]  # nothing, builder, shower-crew, toilet-crew, kitchen-crew
 resource_roles_map = [
-    [0], # no role, no resource
+    [0],  # no role, no resource / started
     [1],  # Bob -> builder
     [2, 4],  # Sandy -> shower-crew, kitchen-crew
     [4],  # Kay -> kitchen-crew
     [3],  # Tina -> toilet-crew
 ]
 roles_resources_map = [
-    [0], # no role, no resource
-    [1], # Builder: Bob
-    [2], # Shower-Crew: Sandy
-    [4], # Toilet-Crew: Tina
-    [2,3] # Kitchen-Crew: Sandy, Kay
+    [0],  # no role, no resource / started
+    [1],  # Builder: Bob
+    [2],  # Shower-Crew: Sandy
+    [4],  # Toilet-Crew: Tina
+    [2, 3],  # Kitchen-Crew: Sandy, Kay
 ]
 
 #####################################
@@ -70,7 +73,8 @@ TIMESLOTS = range(first_time, last_time + 1)
 # resources
 RESOURCES = resource_ids
 
-# we have a decision variable whether the activity is done in the room at the time slot for all possibilities
+# we have a decision variable whether the activity is done in the room at the time slot with this resource for all possibilities
+# actions[t][a][i][0] symbolizes whether the activity is started at time t in room i
 actions = pl.LpVariable.dicts(
     "Action", (TIMESLOTS, ACTIVITIES, INSTANCES, RESOURCES), cat=pl.LpBinary
 )
@@ -118,15 +122,15 @@ for t in TIMESLOTS:
 for t in TIMESLOTS:
     for a in ACTIVITIES:
         for i in INSTANCES:
-            for r in RESOURCES:
-                # endtime is the last time a task is done
-                prob += actions[t][a][i][r] * t <= endtime
-                # starttime is the first time a task is done
-                # if task a is not done at t, the value is set to last_tim with 1-0*last_time and thus satisfies the constraint because last_time >= starttime
-                prob += (
-                    actions[t][a][i][r] * t + (1 - actions[t][a][i][r]) * last_time
-                    >= starttime
-                )
+            # endtime is the last time a task is done
+            prob += actions[t][a][i][0] * t <= endtime
+            # starttime is the first time a task is done
+            # if task a is not done at t, the value is set to last_tim with 1-0*last_time and thus satisfies the constraint because last_time >= starttime
+            prob += (
+                actions[t][a][i][0] * t + (1 - actions[t][a][i][0]) * last_time
+                >= starttime
+            )
+
 
 # end task is done at endtime and only then
 prob += (
@@ -152,106 +156,77 @@ prob += (
 for t in TIMESLOTS:
     # in every room there can be max 1 real activity at a time
     for i in INSTANCES:
-        prob += (
-            pl.lpSum(actions[t][a][i][role_requirement[a]] for a in ACTIVITIES) <= 1
-        )
+        prob += pl.lpSum(actions[t][a][i][0] for a in ACTIVITIES) <= 1
 
     # all real activities only affect one room
     for a in range(start_activity + 1, end_activity):
-        prob += (
-            pl.lpSum(actions[t][a][i][role_requirement[a]] for i in INSTANCES) <= 1
-        )
+        prob += pl.lpSum(actions[t][a][i][0] for i in INSTANCES) <= 1
 
 ### durations are considered -> if an activity is started at t with i, it blocks the next time slots during it's duration
 for time in TIMESLOTS:
     for act in ACTIVITIES:
         for i in INSTANCES:
             # duration can not exeed time limit if a started at t
-            prob += (time + duration[act]) * actions[time][act][i][
-                role_requirement[act]
-            ] <= endtime + 1
+            prob += (time + duration[act]) * actions[time][act][i][0] <= endtime + 1
             # if a started and duration > 1, the next time slots are also blocked
             for j in range(1, min((duration[act] - 1, last_time - time))):
                 for a in ACTIVITIES:
-                    prob += (
-                        actions[time + j][a][i][role_requirement[a]]
-                        <= 1 - actions[time][act][i][role_requirement[act]]
-                    )
-            # TODO: also block resources
+                    prob += actions[time + j][a][i][0] <= 1 - actions[time][act][i][0]
 
 ### resource constraints ###
 for t in TIMESLOTS:
     # each resource is only used once at each point of time
-    for r in RESOURCES:
+    for r in RESOURCES[1:]:
         prob += (
             pl.lpSum(actions[t][a][i][r] for a in ACTIVITIES for i in INSTANCES) <= 1
         )
     # each activity is executed with its required resources
-    for a in ACTIVITIES:
+    for a in ACTIVITIES[start_activity + 1 : end_activity]:
         for i in INSTANCES:
-            # TODO: fix this. should be consumption or 0
             # consumption is satisfied
-            prob += pl.lpSum(actions[t][a][i][r] for r in RESOURCES) == resource_consumption[a]
+            # actions[t][a][i][0] symbolizes whether the activity is started at time t in room i
+            prob += (
+                pl.lpSum(actions[t][a][i][r] for r in RESOURCES[1:])
+                == resource_consumption[a] * actions[t][a][i][0]
+            )    
             # satisfied only with required role
-            prob += pl.lpSum(actions[t][a][i][r] for r in roles_resources_map[role_requirement[a]]) == resource_consumption[a]
+            prob += pl.lpSum(
+                actions[t][a][i][r] for r in roles_resources_map[role_requirement[a]]
+            ) == pl.lpSum(actions[t][a][i][r] for r in RESOURCES[1:])
 
 ### activity / data dependencies ###
-# TODO: fix resources. maybe build sum and divide by requirement to get 0 / 1
 for i in INSTANCES:
     for time in TIMESLOTS:
         # start before build room (redundant)
         prob += pl.lpSum(
-            actions[t][start_activity][i][role_requirement[start_activity]]
-            for t in range(0, time)
-        ) >= pl.lpSum(
-            actions[t][build_room][i][role_requirement[build_room]]
-            for t in range(0, time)
-        )
+            actions[t][start_activity][i][0] for t in range(0, time)
+        ) >= pl.lpSum(actions[t][build_room][i][0] for t in range(0, time))
 
         # build room or install toilet before install shower
         prob += pl.lpSum(
-            actions[t][build_room][i][role_requirement[build_room]]
-            - actions[t][install_toilet_b][i][role_requirement[install_toilet_b]]
+            actions[t][build_room][i][0] - actions[t][install_toilet_b][i][0]
             for t in range(0, time)
-        ) >= pl.lpSum(
-            actions[t][install_shower_b][i][role_requirement[install_shower_b]]
-            for t in range(0, time)
-        )
+        ) >= pl.lpSum(actions[t][install_shower_b][i][0] for t in range(0, time))
         prob += pl.lpSum(
-            actions[t][install_toilet_b][i][role_requirement[install_toilet_b]]
-            for t in range(0, time)
-        ) >= pl.lpSum(
-            actions[t][install_shower_t][i][role_requirement[install_shower_t]]
-            for t in range(0, time)
-        )
+            actions[t][install_toilet_b][i][0] for t in range(0, time)
+        ) >= pl.lpSum(actions[t][install_shower_t][i][0] for t in range(0, time))
 
         # build room or install shower before install toilet
         prob += pl.lpSum(
-            actions[t][build_room][i][role_requirement[build_room]]
-            - actions[t][install_shower_b][i][role_requirement[install_shower_b]]
+            actions[t][build_room][i][0] - actions[t][install_shower_b][i][0]
             for t in range(0, time)
-        ) >= pl.lpSum(
-            actions[t][install_toilet_b][i][role_requirement[install_toilet_b]]
-            for t in range(0, time)
-        )
+        ) >= pl.lpSum(actions[t][install_toilet_b][i][0] for t in range(0, time))
         prob += pl.lpSum(
-            actions[t][install_shower_b][i][role_requirement[install_shower_b]]
-            for t in range(0, time)
-        ) >= pl.lpSum(
-            actions[t][install_toilet_s][i][role_requirement[install_toilet_s]]
-            for t in range(0, time)
-        )
+            actions[t][install_shower_b][i][0] for t in range(0, time)
+        ) >= pl.lpSum(actions[t][install_toilet_s][i][0] for t in range(0, time))
 
         # build room before install kitchen (and not bathroom)
         prob += pl.lpSum(
-            actions[t][build_room][i][role_requirement[build_room]]
-            - actions[t][install_toilet_b][i][role_requirement[install_toilet_b]]
-            - actions[t][install_shower_b][i][role_requirement[install_shower_b]]
+            actions[t][build_room][i][0]
+            - actions[t][install_toilet_b][i][0]
+            - actions[t][install_shower_b][i][0]
             for t in range(0, time)
-        ) >= pl.lpSum(
-            actions[t][install_kitchen][i][role_requirement[install_kitchen]]
-            for t in range(0, time)
-        )
+        ) >= pl.lpSum(actions[t][install_kitchen][i][0] for t in range(0, time))
 
 ########################
 ### define the goals ###
@@ -260,18 +235,13 @@ for i in INSTANCES:
 ### goal = empty rooms, kitchens, bathrooms
 # kitchens
 prob += (
-    pl.lpSum(
-        actions[t][install_kitchen][i][role_requirement[install_kitchen]]
-        for t in TIMESLOTS
-        for i in INSTANCES
-    )
+    pl.lpSum(actions[t][install_kitchen][i][0] for t in TIMESLOTS for i in INSTANCES)
     >= kitchens_to_build
 )
 # bathrooms
 prob += (
     pl.lpSum(
-        actions[t][install_toilet_s][i][role_requirement[install_toilet_s]]
-        + actions[t][install_shower_t][i][role_requirement[install_shower_t]]
+        actions[t][install_toilet_s][i][0] + actions[t][install_shower_t][i][0]
         for t in TIMESLOTS
         for i in INSTANCES
     )
@@ -280,14 +250,11 @@ prob += (
 # empty rooms
 prob += (
     pl.lpSum(
-        pl.lpSum(
-            actions[t][build_room][i][role_requirement[build_room]]
-            for t in TIMESLOTS
-        )
+        pl.lpSum(actions[t][build_room][i][0] for t in TIMESLOTS)
         - pl.lpSum(
-            actions[t][install_kitchen][i][role_requirement[install_kitchen]]
-            + actions[t][install_toilet_b][i][role_requirement[install_toilet_b]]
-            + actions[t][install_shower_b][i][role_requirement[install_shower_b]]
+            actions[t][install_kitchen][i][0]
+            + actions[t][install_toilet_b][i][0]
+            + actions[t][install_shower_b][i][0]
             for t in TIMESLOTS
         )
         for i in INSTANCES
